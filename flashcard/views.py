6564fb073f3459403a8f1d06447a50f6d615b51a
jonas-thiago <146122741+jonas-thiago@users.noreply.github.com>
2024-01-19 16:55:17 -0300

    version5
    completo só falta terminar as tarefas propostas

--- flashcard/views.py
+++ flashcard/views.py
@@ -143,3 +143,25 @@ def responder_flashcard(request, id):
     flashcard_desafio.save()
 
     return redirect(f'/flashcard/desafio/{desafio_id}')
+
+def relatorio(request, id):
+    desafio = Desafio.objects.get(id=id)
+
+    acertos = desafio.flashcards.filter(acertou=True).count()
+    erros = desafio.flashcards.filter(acertou=False).count()
+
+    dados = [acertos, erros]
+
+    categorias = desafio.categoria.all()
+    name_categoria = []
+    for i in categorias:
+        name_categoria.append(i.nome)
+    
+    dados2= []
+    for categoria in categorias:
+        dados2.append(desafio.flashcards.filter(flashcard__categoria=categoria).filter(acertou=True).count())
+    # Criar os ranking
+        
+    return render(request, 'relatorio.html', {'desafio': desafio, 'dados': dados, 
+                                              'categorias': name_categoria, 'dados2': dados2})
+
